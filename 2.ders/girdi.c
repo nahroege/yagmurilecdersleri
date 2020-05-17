4ecc693e1ce2ba993a21788d3a602a640ccdf2b7
ege <you@example.com>
2020-05-17 13:35:10 +0300

    gün gün ayrıldı

---
+++ 2.ders/girdi.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main( void )
+{
+	int sayi;
+	printf("Değer giriniz> ");
+	scanf("%d",&sayi);
+	printf("Girilen değer: %d\n",sayi);
+	return 0;
+}
---
+++ 2.ders/giricbekleyen.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main( void )
+{
+   // Hello World yazar.
+   printf( "Hello World" );
+   // Sizden herhangi bir giriş bekler.
+   // Böylece program çalışıp, kapanmaz.
+   getchar( );
+   return 0;
+}
